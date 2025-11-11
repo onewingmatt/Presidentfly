9dafe01ca92fa2f107db04d49bef7fa4b7884b51
onewingmatt <onewing@gmail.com>
2025-11-10 21:25:41 -0400

    bigfix

--- app.py
+++ app.py
@@ -1114,13 +1114,12 @@ def on_play(data):
             socketio.emit('update', {'state': game.get_state()}, to=gid)
             current = game.get_current_player()
             if current and current.is_cpu:
-                    # Play log event
-    socketio.emit('play_made', {'player_name': player.name, 'cards': ', '.join([f"{c.rank.value[1]}{c.suit.value}" for c in meld]), 'action': 'play'}, to=gid)
-
-    # Your turn event for human player
-    curr = game.get_current_player()
-    if curr and not curr.is_cpu:
-        socketio.emit('your_turn', {'player_id': curr.player_id}, to=gid)
+                    socketio.emit('play_made', {'player_name': player.name, 'cards': ', '.join([f"{c.rank.value[1]}{c.suit.value}" for c in meld]), 'action': 'play'}, to=gid)
+    try:
+        curr = game.get_current_player()
+        if curr and not curr.is_cpu:
+            socketio.emit('your_turn', {'player_id': curr.player_id}, to=gid)
+    except: pass
 
     socketio.emit('cpu_turn', {}, to=gid)
         return
@@ -1168,9 +1167,7 @@ def on_pass():
         return
     
     save_game_to_disk(game)
-        # Pass event
-    socketio.emit('play_made', {'player_name': player.name, 'cards': 'Pass', 'action': 'pass'}, to=gid)
-
+        socketio.emit('play_made', {'player_name': player.name, 'cards': 'Pass', 'action': 'pass'}, to=gid)
     socketio.emit('update', {'state': game.get_state()}, to=gid)
     
     if result.get('round_over'):
